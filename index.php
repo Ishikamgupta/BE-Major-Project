49a9693ed034dd969c33cd553691fe0df50c1855
Ishika Gupta <71513488+Ishikamgupta@users.noreply.github.com>
2023-05-05 20:54:47 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,96 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+        integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <style>
+        #ques{
+            min-height: 433px;
+        }
+    </style>
+    <title>Welcome to Discussion Forums</title>
+</head>
+
+<body>
+    <?php include 'partials/_header.php';?>
+    <?php include 'partials/_dbconnect.php';?>
+
+    <div id="carouselExampleIndicators" class="carousel slide">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="https://source.unsplash.com/2400x700/?programming,code" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="https://source.unsplash.com/2400x700/?placement,job" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="https://source.unsplash.com/2400x700/?students,learning" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+
+    <div class="container my-3">
+        <h2 class="text-center my-3">Browse Discussion Categories</h2>
+        <div class="row">
+
+             <!-- Fetch all the categories and use a loop to iterate through categories -->
+         <?php 
+         $sql = "SELECT * FROM `categories`"; 
+         $result = mysqli_query($conn, $sql);
+         while($row = mysqli_fetch_assoc($result)){
+          // echo $row['category_id'];
+          // echo $row['category_name'];
+          $id = $row['category_id'];
+          $cat = $row['category_name'];
+          $desc = $row['category_description'];
+          echo '<div class="col-md-4 my-2">
+                  <div class="card" style="width: 18rem;">
+                      <img src="https://source.unsplash.com/200x100/?'.$id. '.jpg" class="card-img-top" alt="image for this category">
+                      <div class="card-body">
+                          <h5 class="card-title"><a href="threadlist.php?catid=' . $id . '">' . $cat . '</a></h5>
+                          <p class="card-text">' . substr($desc, 0, 70). '... </p>
+                          <a href="threadlist.php?catid=' . $id . '" class="btn btn-primary">View Threads</a>
+                      </div>
+                  </div>
+                </div>';
+         } 
+         ?>
+       
+        </div>
+    </div>
+
+    <?php include 'partials/_footer.php';?>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
+        integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
